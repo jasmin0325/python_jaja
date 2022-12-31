561aa63bab31ba5282c9cc2f9c5d434bad2f273b
ijjamminn <lee.ja.min325@gmail.com>
2022-12-31 10:11:03 +0900

    test

---
+++ day1/test.py
@@ -0,0 +1,17 @@
+# print("hello python")
+# i=0
+# print(i)
+# j=10
+# print(i+j)
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange%apple)
+print(orange/apple)
+print(2**2)
+print(3**3)
+print(10/3)
+print(10%3)
+print(orange//apple)
