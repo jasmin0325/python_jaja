4bac60d014513d1cf0999cb835b4c338706e3bc8
ijjamminn <lee.ja.min325@gmail.com>
2023-01-28 10:12:53 +0900

    반복문 복습

---
+++ day3/복습.py
@@ -0,0 +1,36 @@
+#100~110중 홀수만 출력
+# a=100
+# while a<111:
+#     if a%2!=0:
+#         print(a)
+#     a+=1
+# for i in range(100,110):
+#     if i%2!=0:
+#         print(i)
+#100~200중 2와3과5의 공배수 출력
+# a=100
+# while a<201:
+#     if a%2==0:
+#         if a%3==0:
+#             if a%5==0:
+#                 print(a)
+#     a+=1
+# for i in range(100,201):
+#     if i%2==0:
+#         if i%3==0:
+#             if i%5==0:
+#                 print(i)
+#1~10중 3의 배수가 아닌 수들의 합
+# a=0
+# 합=0
+# while a<11:
+#     if a%3!=0:
+#         합+=a
+#     a+=1
+# print(합)
+# sum=0
+# for i in range(11):
+#     if i%3!=0:
+#         sum+=i
+# print(sum)
+
