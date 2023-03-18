6462deea6d477d4fc1002c2023369476f317f5e4
ijjamminn <lee.ja.min325@gmail.com>
2023-03-18 10:05:26 +0900

    plot

---
+++ day8/plot2.py
@@ -0,0 +1,14 @@
+# import matplotlib.pyplot as plt
+# fruits= ["apple","banana","cherry","orange"]
+# counts=[4,7,6,9]
+# figure,axes=plt.subplots(1,2)
+# axes[0].bar(fruits,counts)
+# axes[1].bar(fruits,counts)
+# plt.show()
+# import matplotlib.pyplot as plt
+# fruits= ["apple","banana","cherry","orange"]
+# counts=[4,7,6,9]
+# figure,axes=plt.subplots(1,2)
+# axes[0].plot([5,0],[0,5])
+# axes[1].plot([0,5],[0,5])
+# plt.show()
